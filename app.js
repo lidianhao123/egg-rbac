c6f2f8aa6b455bc6e75ea493a61824f5f99048a7
dainli <dainli@outlook.com>
2017-11-09 17:29:57 +0800

    add rbac to app

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const assert = require('assert');
+
 module.exports = app => {
-  console.log('app.config.env =', app.config.env);
+  // console.log('app.config.env =', app.config.env);
+  const mongoose = app.mongoose;
+  assert(mongoose, 'mongoose instance not exists');
+  app.rbac = require('./lib/RBAC')(app);
 };
