172cd7006de3c41ef0e5d3f8d7b3b1b3f5773318
dainli <dainli@outlook.com>
2017-11-09 23:25:03 +0800

    add plugin middleware to app

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ module.exports = app => {
   const mongoose = app.mongoose;
   assert(mongoose, 'mongoose instance not exists');
   app.rbac = require('./lib/RBAC')(app);
+
+  // https://eggjs.org/zh-cn/basics/middleware.html#在框架和插件中使用中间件
+  // 将 rbac 中间件放到 coreMiddleware 最后
+  app.config.coreMiddleware.push('rbac');
 };
