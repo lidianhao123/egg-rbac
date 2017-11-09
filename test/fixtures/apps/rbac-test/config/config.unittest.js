61722701a153bb8ba6544443acb66425a326aae4
dainli <dainli@outlook.com>
2017-11-09 17:37:50 +0800

    add fixture test rbac config

--- test/fixtures/apps/rbac-test/config/config.unittest.js
+++ test/fixtures/apps/rbac-test/config/config.unittest.js
@@ -7,3 +7,17 @@ exports.mongoose = {
   url: 'mongodb://127.0.0.1/testrbac',
   options: {},
 };
+
+exports.rbac = {
+  initOnStart: true, // default false
+  /**
+   * @param {object} ctx - egg context object
+   * @return {object} promise, if resolve data is falsy, no role
+   */
+  * getRoleName(ctx) {
+    if (ctx.query.role) {
+      return Promise.resolve(ctx.query.role);
+    }
+    return Promise.resolve('');
+  },
+};
---
+++ test/fixtures/apps/rbac-test/config/rbac.js
@@ -0,0 +1,19 @@
+'use strict';
+
+exports.permissions = [
+  // action_resource
+  { name: 'create_user', alias: '创建用户' },
+  { name: 'delete_user', alias: '删除用户' },
+  { name: 'query_user', alias: '查询用户' },
+  { name: 'edit_user', alias: '修改用户' },
+
+  { name: 'create_article', alias: '创建文章' },
+  { name: 'delete_article', alias: '删除文章' },
+  { name: 'create_article', alias: '查询文章' },
+  { name: 'edit_article', alias: '修改文章' },
+];
+
+exports.roles = [
+  { name: 'admin', alias: '管理员', grants: exports.permissions.map(item => item.name) },
+  { name: 'editer', alias: '编辑', grants: [ 'create_article', 'delete_article', 'create_article', 'edit_article' ] },
+];
