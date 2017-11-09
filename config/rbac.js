1da9fefc6a72cc2f7df4b37b4fe47f2e1ff18045
dainli <dainli@outlook.com>
2017-11-09 17:33:31 +0800

    add default permission file

---
+++ config/rbac.js
@@ -0,0 +1,13 @@
+'use strict';
+
+exports.permissions = [
+  // action_resource
+  // { name: 'create_user', alias: '创建用户' },
+  // { name: 'delete_user', alias: '删除用户' },
+  // { name: 'query_user', alias: '查询用户' },
+  // { name: 'edit_user', alias: '修改用户' },
+];
+
+exports.roles = [
+  // { name: 'admin', alias: '管理员', grants: exports.permissions.map(item => item.name) },
+];
