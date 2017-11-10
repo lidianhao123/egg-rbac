1e16f4280e5b32141ebf1c820be8804583ac5bb2
dainli <dainli@outlook.com>
2017-11-10 09:18:12 +0800

    add test session feature

--- test/rbac.test.js
+++ test/rbac.test.js
@@ -18,8 +18,25 @@ describe('test/rbac.test.js', () => {
   afterEach(mm.restore);
 
   it('should GET /admin 200 when role is admin', function* () {
-    return app.httpRequest()
-      .get('/admin?role=admin')
+    const roleName = 'admin';
+    const permissions = yield app.rbac.getRolePermission(roleName);
+
+    // test role equal to 'admin'
+    yield app.httpRequest()
+      .get('/admin?role=' + roleName)
+      // .expect(200, 'success')
+      .expect(200, 'success', {
+        session: {
+          permission: { roleName, permissions },
+        },
+      });
+
+    // with session info so no need role
+    app.mockSession({
+      permission: { roleName, permissions },
+    });
+    yield app.httpRequest()
+      .get('/admin')
       .expect(200, 'success');
   });
 
