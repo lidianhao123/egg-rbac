163a0c5b092ce6f557b65279becf19651f50ddf1
dainli <dainli@outlook.com>
2017-11-24 15:10:59 +0800

    inner middleware remove session cache

--- app/middleware/rbac.js
+++ app/middleware/rbac.js
@@ -5,21 +5,10 @@ const Role = require('../../lib/role');
 // options: 中间件的配置项，框架会将 app.config[${middlewareName}] 传递进来
 module.exports = options => {
   return function* rbac(next) {
-    // this is instance of Context
-    // TODO support session
-    let permissions;
-    if (!this.session.permission) {
-      const roleName = yield options.getRoleName(this);
-      if (roleName) {
-        permissions = yield this.app.rbac.getRolePermission(roleName);
-        this.role = new Role(roleName, permissions);
-        this.session.permission = {
-          roleName,
-          permissions,
-        };
-      }
-    } else {
-      this.role = new Role(this.session.permission.roleName, this.session.permission.permissions);
+    const roleName = yield options.getRoleName(this);
+    if (roleName) {
+      const permissions = yield this.app.rbac.getRolePermission(roleName);
+      this.role = new Role(roleName, permissions);
     }
 
     yield next;
